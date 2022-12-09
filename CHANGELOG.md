771a3f676cf4b8c96f76711121dd23002b56cf48
skhsiao <steven-KP.hsiao@nanshan.com.tw>
2022-12-09 17:16:06 +0800

    Fix the problem that the title widget cannot be expanded

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 
 
+## 1.0.4
+
+- Fix the problem that the title widget cannot be expanded
+
 ## 1.0.3
 
 - Initial Release
--- lib/src/ExpandableWidget.dart
+++ lib/src/ExpandableWidget.dart
@@ -189,11 +189,11 @@ class _ExpandableWidgetState extends State<ExpandableWidget> {
   Widget getTitle() {
     List<Widget> titleRow = [];
     if (widget.trailingPosition == TrailingPosition.right) {
-      titleRow.add(widget.title);
+      titleRow.add(Expanded(child: widget.title));
       titleRow.add(getTrailing());
     } else {
       titleRow.add(getTrailing());
-      titleRow.add(widget.title);
+      titleRow.add(Expanded(child: widget.title));
     }
 
     return GestureDetector(
